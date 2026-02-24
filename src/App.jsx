02bec5102224e8e2d277bc1821a9d87acaa2b383
Cirko023 <ciricjovan27@gmail.com>
2026-02-24 18:00:32 +0100

    Kreiranje REACT JS
    Kreirana REACTJS aplikacija i dodat tailwind kao framework za css.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-purple-600">
+       NOVINARSKI PORTAL SA TAILWINDOM JUPI 🎉
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
